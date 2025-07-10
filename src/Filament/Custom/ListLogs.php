80b51e1b21814ec57a33bf0fa749eb260fc4ce54
atendwa <atendwa@gmail.com>
2025-07-11 00:50:15 +0300

    chore: minor fix

--- src/Filament/Custom/ListLogs.php
+++ src/Filament/Custom/ListLogs.php
@@ -2,11 +2,11 @@
 
 declare(strict_types=1);
 
-namespace Atendwa\Msingi\Filament\Pages;
+namespace Atendwa\Msingi\Filament\Custom;
 
 use Atendwa\Msingi\Filament\Clusters\Insights;
 
-class ViewLog extends \Boquizo\FilamentLogViewer\Pages\ViewLog
+class ListLogs extends \Boquizo\FilamentLogViewer\Pages\ListLogs
 {
     public static function getNavigationGroup(): ?string
     {
@@ -16,5 +16,10 @@ class ViewLog extends \Boquizo\FilamentLogViewer\Pages\ViewLog
     public static function getCluster(): ?string
     {
         return Insights::class;
+    }
+
+    public static function canAccess(): bool
+    {
+        return auth()->user()?->can('viewLogs') ?? false;
     }
 }
