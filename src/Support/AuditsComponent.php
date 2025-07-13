b7aa5433ae7cc9b7b987695bb3221dff8058083e
atendwa <atendwa@gmail.com>
2025-07-13 22:41:39 +0300

    chore: minor fix

--- src/Support/AuditsComponent.php
+++ src/Support/AuditsComponent.php
@@ -3,18 +3,21 @@
 namespace Atendwa\Msingi\Support;
 
 use Filament\Forms\Components\Section;
+use Illuminate\Database\Eloquent\Model;
 
 class AuditsComponent
 {
     public static function form(): Section
     {
-        return Section::make('Audits')->collapsible()->collapsed()
-            ->persistCollapsed()->columns(1)->schema([]);
+        return Section::make('Audits')->collapsible()->collapsed()->persistCollapsed()->columns(1)
+            ->visible(fn (?Model $model) => $model && permission('audit', model: $model))
+            ->schema([]);
     }
 
     public static function infolist(): \Filament\Infolists\Components\Section
     {
-        return \Filament\Infolists\Components\Section::make('Audits')->collapsible()
-            ->collapsed()->persistCollapsed()->columns(1)->schema([]);
+        return \Filament\Infolists\Components\Section::make('Audits')->collapsible()->persistCollapsed()
+            ->visible(fn (?Model $model) => $model && permission('audit', model: $model))->collapsed()
+            ->columns(1)->schema([]);
     }
 }
