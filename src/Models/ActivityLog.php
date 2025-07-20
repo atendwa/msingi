b5c4c4068985b33393a0ae0c40d3bead7b2ebb68
atendwa <atendwa@gmail.com>
2025-07-20 22:39:27 +0300

    chore: minor fix

--- src/Models/ActivityLog.php
+++ src/Models/ActivityLog.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 namespace Atendwa\Msingi\Models;
 
-use Atendwa\Support\Contracts\HasFilamentTabs;
+use Atendwa\Filakit\Concerns\UsesFilamentTabs;
+use Atendwa\Filakit\Contracts\HasFilamentTabs;
 use Filament\Support\Contracts\HasIcon;
 use Spatie\Activitylog\Models\Activity;
 
 class ActivityLog extends Activity implements HasFilamentTabs, HasIcon
 {
+    use UsesFilamentTabs;
+
     public string $icon = 'heroicon-o-document-text';
 
     protected $table = 'activity_log';
