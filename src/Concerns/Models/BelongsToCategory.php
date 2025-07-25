a3980f9b3e33b6f49fa3e19e58b9b1b9e280b12c
atendwa <atendwa@gmail.com>
2025-07-25 22:33:35 +0300

    chore: minor fix

---
+++ src/Concerns/Models/BelongsToCategory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Atendwa\Msingi\Concerns\Models;
+
+use Core\Models\Category;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+trait BelongsToCategory
+{
+    /**
+     * @return BelongsTo<Category, $this>
+     */
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class)->withoutGlobalScopes();
+    }
+
+    public function categoryId(): ?int
+    {
+        $id = $this->getAttribute('category_id');
+
+        return match (is_numeric($id)) {
+            true => (int) $id,
+            false => null,
+        };
+    }
+}
