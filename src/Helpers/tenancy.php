b8d3a225b7a83af604f0b8fd5991137065ed3da1
atendwa <atendwa@gmail.com>
2025-07-27 21:14:09 +0300

    chore: minor fix

--- src/Helpers/tenancy.php
+++ src/Helpers/tenancy.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Atendwa\Msingi\Models\Tenant;
 use Atendwa\Msingi\Support\MsingiCacheKeys;
+use Filament\Facades\Filament;
 
 if (! function_exists('systemTenant')) {
     function systemTenant(): Tenant
@@ -36,5 +37,19 @@ if (! function_exists('tenantID')) {
         }
 
         return asInteger($id);
+    }
+}
+
+if (! function_exists('inDepartmentTenant')) {
+    function inDepartmentTenant(string $column = 'department_short_name'): bool
+    {
+        return Filament::getTenant()?->getAttribute($column) === auth()->user()?->getAttribute($column);
+    }
+}
+
+if (! function_exists('isSystemStaff')) {
+    function isSystemStaff(): bool
+    {
+        return auth()->user()?->isSystemStaff() ?? false;
     }
 }
